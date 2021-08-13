a15ac54aaf6d4283526864917b9b6ac57a20d6b8
Albert Fredy <albertfredy007@gmail.com>
2021-08-13 15:46:54 +0530

    added 2 randam quotes in a single line

--- get-quote.py
+++ get-quote.py
@@ -3,10 +3,10 @@ def main():
      f = open("quotes.txt")
      quotes = f.readlines()
      f.close()
-     last = 13
+     last = len(quotes) - 1
      rnd = random.randint(0, last)
 
-     print(quotes[rnd])
+     print(random.sample(quotes,2))
 
 if __name__== "__main__":
   main()
